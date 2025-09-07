2cb7d762b57082d7445be8c638f3df901fc812c0
Karen Gasparyan <karen@callx.com>
2025-09-07 13:36:03 +0400

    homework 2

---
+++ src/homework/homework2/Homework2.java
@@ -0,0 +1,10 @@
+package homework.homework2;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+        if(10>6){
+            System.out.println("true");
+        }
+    }
+}
